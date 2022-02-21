735560db9fac6d5e8b85efc21fa2e211e0ec5bb3
Maxime Arquilliere <maxime.arq@gmail.com>
2022-02-21 17:23:36 +0100

    new metaverse method and view created

--- app/controllers/metaverses_controller.rb
+++ app/controllers/metaverses_controller.rb
@@ -1,2 +1,22 @@
 class MetaversesController < ApplicationController
+
+  def new
+    @metaverse = Metaverse.new
+  end
+
+  def create
+   @metaverse = Metaverse.new(metaverse_params)
+   if @metaverse.save
+     redirect_to metaverse_path(@metaverse)
+    else
+     render :new
+    end
+  end
+
+  private
+
+  def metaverse_params
+   params.require(:metaverse).permit(:title, :description, :price)
+  end
+
 end
