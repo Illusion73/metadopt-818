f84ebb5adf2f370e6a9b9a98055bc1127619fe03
Josephine <josephine.lazzaris@gmail.com>
2022-02-21 17:25:24 +0100

    edit update method in metaverse controller

--- app/controllers/metaverses_controller.rb
+++ app/controllers/metaverses_controller.rb
@@ -1,2 +1,22 @@
 class MetaversesController < ApplicationController
+  before_action :find_by_id, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @metaverse.update(metaverse_params)
+
+    redirect_to metaverse_path(@metaverse)
+  end
+
+  private
+
+  def metaverse_params
+    params.require(:metaverse).permit(:title, :description, :price)
+  end
+
+  def find_by_id
+    @metaverse = Metaverse.find(params[:id])
+  end
 end
