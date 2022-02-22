349abec431e8e7cccb3d91aba44408424f55b287
Josephine <josephine.lazzaris@gmail.com>
2022-02-22 12:12:29 +0100

    update new create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @metaverse = Metaverse.find(params[:metaverse_id])
+    # @user = User.find(params[:user_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @metaverse = Metaverse.find(params[:metaverse_id])
+    @booking.metaverse = @metaverse
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:metaverse_id, :start_at, :end_at, :user_id)
+    end
 end
