bca24e43cde8bff63d6870bbb2cf9c59bac76e4a
fassi fihri mohammed <mohammedfassifihri09@gmail.com>
2022-02-22 16:19:16 +0100

    i add an accordion to the profil show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @metaverses = @user.metaverses
-    @metaverses = Metaverse.where(user: current_user)
-
-    @bokings = Booking.where(user: current_user)
+    @bookings = @user.bookings
     @user_booked_metaverses = @user.user_booked_metaverses
     @owner_booked_metaverses = @user.owner_booked_metaverses
     # @booked_metaverses2 = Booking.joins(:metaverse).where(metaverse: { user_id: @user.id } ) IDEM CELLE AVANT
