d49ba168f05c2a43874b0aea19946861b4d4c4e3
fassi fihri mohammed <mohammedfassifihri09@gmail.com>
2022-02-23 11:12:56 +0100

    add dropdown list on navbar, modified link in profil & add some destroy all on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+Review.destroy_all
+Booking.destroy_all
 Metaverse.destroy_all
 User.destroy_all
 
